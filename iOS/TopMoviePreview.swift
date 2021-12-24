66ed3af6ba7f8cc8e8677cba4505ee28345bdbc6
Victor Mauricio Cota Vidaurre <vicmauricv@hotmail.com>
2021-12-23 20:00:37 -0400

    Adding gradient to the TopMoviePreview image

--- iOS/TopMoviePreview.swift
+++ iOS/TopMoviePreview.swift
@@ -71,6 +71,7 @@ struct TopMoviePreview: View {
                     Spacer()
                 }
             }
+            .background(LinearGradient(colors: [Color.black.opacity(0.0), Color.black.opacity(0.95)], startPoint: .top, endPoint: .bottom))
         }
         .foregroundColor(.white)
     }
